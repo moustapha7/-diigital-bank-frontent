8a6ff06238351f90f4cbfb57fe6104f9cf940c2b
Moustapha DIEDHIOU <moustapha.diedhiou@talan.com>
2024-01-01 11:35:18 +0100

    ajout operation bank et CRUD customer

---
+++ src/app/customer-accounts/customer-accounts.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Customer } from '../model/customer.model';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CustomerService } from '../services/customer.service';
+
+@Component({
+  selector: 'app-customer-accounts',
+  standalone: true,
+  imports: [CommonModule,HttpClientModule,ReactiveFormsModule],
+  providers:  [ CustomerService ],
+  templateUrl: './customer-accounts.component.html',
+  styleUrl: './customer-accounts.component.css'
+})
+
+
+export class CustomerAccountsComponent implements OnInit {
+
+
+  customerId! : string;
+  customer! : Customer;
+
+  constructor(private route : ActivatedRoute, private router : Router){
+    this.customer = this.router.getCurrentNavigation()?.extras.state as Customer;
+  }
+
+  ngOnInit(): void {
+    this.customerId = this.route.snapshot.params['id'];
+  }
+
+}
